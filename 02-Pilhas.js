6c3a230aa4a9098b42727437a2f0dd5141ae5bef
Marcelo Santos <rocha.mer21@gmail.com>
2022-01-12 16:09:08 -0300

    Pilha

--- 02-Pilhas.js
+++ 02-Pilhas.js
@@ -1,1 +1,51 @@
 /* Pilhas são estruturas de dados que armazenam os elementos em um formato sequencial, empilhando um item acima do outro (imagine uma pilha de pratos, por exemplo). Estas estruturas permitem “empilhar” os itens que serão armazenados e “desempilhar” estes elementos da pilha quando precisarmos removê-lo. */
+
+function Stack(params) {
+  const items = []
+
+  this.push = function (element) {
+    // adiciona um novo item a pilha
+    items.push(element)
+  }
+
+  this.pop = function () {
+    // remove um item do topo da pilha
+    return items.pop()
+  }
+
+  this.peek = function () {
+    // devolve o elemento que esta no topo da pilha
+    return items[items.length - 1]
+  }
+
+  this.isempty = function () {
+    // informa se a pilha esta vazia
+    return items.length === 0
+  }
+
+  this.clear = function () {
+    // limpa a pilha
+    items = []
+  }
+
+  this.size = function () {
+    // informa o tamanho da pilha
+    return items.length
+  }
+
+  this.print = function () {
+    // imprime a pilha
+    console.log(items.toString())
+  }
+}
+
+const pilha = new Stack()
+pilha.push('Marcelo')
+pilha.push('Gabriel')
+pilha.push('Magdalena')
+pilha.push('Ezequias')
+pilha.push('Alice')
+
+console.log(`Elemento do topo da pilha: ${pilha.peek()}`)
+
+console.log(`Tamanho da pilha: ${pilha.size()}`)
